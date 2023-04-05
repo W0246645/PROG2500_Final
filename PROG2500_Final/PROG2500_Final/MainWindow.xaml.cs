624c4b36341312ca3fd0a8576b1642651321bd14
Matthew Covey <90411582+W0246645@users.noreply.github.com>
2023-04-05 20:50:59 -0300

    Pages all scaffolded out and mainwindow is roughly styled.

--- PROG2500_Final/PROG2500_Final/MainWindow.xaml.cs
+++ PROG2500_Final/PROG2500_Final/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PROG2500_Final.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,45 @@ namespace PROG2500_Final
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ImdbProjectContext _context = new ImdbProjectContext();
+        public Pages.HomePage HomePage { get; set; }
+        public Pages.MoviesPage MoviesPage { get; set; }
+        public Pages.SeriesPage SeriesPage { get; set; }
+        public Pages.DirectorsPage DirectorsPage { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            HomePage = new Pages.HomePage();
+            MoviesPage = new Pages.MoviesPage();
+            SeriesPage = new Pages.SeriesPage();
+            DirectorsPage = new Pages.DirectorsPage();
+
+            mainFrame.NavigationService.Navigate(HomePage);
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(HomePage);
+        }
+
+        private void MoviesButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(MoviesPage);
+        }
+
+        private void SeriesButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(SeriesPage);
+        }
+
+        private void DirectorsButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(DirectorsPage);
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
