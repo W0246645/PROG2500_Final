e13f0319b878b0348b252ac847605a85d6966107
bvarga0716 <w0432792@nscc.ca>
2023-04-08 18:08:31 -0300

    Movies Page Complete

--- PROG2500_Final/PROG2500_Final/Pages/MoviesPage.xaml.cs
+++ PROG2500_Final/PROG2500_Final/Pages/MoviesPage.xaml.cs
@@ -1,28 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using PROG2500_Final.Data;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PROG2500_Final.Pages
 {
-    /// <summary>
-    /// Interaction logic for MoviesPage.xaml
-    /// </summary>
     public partial class MoviesPage : Page
     {
+        private ImdbProjectContext _context;
+        public ObservableCollection<MovieData> Movies { get; set; }
+
         public MoviesPage()
         {
             InitializeComponent();
+            _context = new ImdbProjectContext();
+            Movies = new ObservableCollection<MovieData>();
         }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadMovies();
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            LoadMovies(SearchTextBox.Text);
+        }
+
+        private void LoadMovies(string searchTitle = null)
+        {
+            Movies.Clear();
+
+            var titles = _context.Titles.Take(500).ToList();
+            if (!string.IsNullOrEmpty(searchTitle))
+            {
+                titles = titles.Where(t => t.PrimaryTitle.Contains(searchTitle)).ToList();
+            }
+
+            foreach (var title in titles)
+            {
+                var movieData = new MovieData
+                {
+                    Title = title.PrimaryTitle
+                };
+
+                // Fetch average rating from Ratings table using titleId
+                var rating = _context.Ratings.FirstOrDefault(r => r.TitleId == title.TitleId);
+                if (rating != null)
+                {
+                    movieData.AverageRating = (double?)rating.AverageRating;
+                }
+
+                // Fetch director's name from Principals table using titleId and job_category
+                var principal = _context.Principals.FirstOrDefault(p => p.TitleId == title.TitleId && p.JobCategory == "director");
+                if (principal != null)
+                {
+                    var name = _context.Names.FirstOrDefault(n => n.NameId == principal.NameId);
+                    if (name != null)
+                    {
+                        movieData.Director = name.PrimaryName;
+                    }
+                }
+
+                Movies.Add(movieData);
+            }
+
+            MoviesListView.ItemsSource = Movies;
+        }
+    }
+
+    public class MovieData
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public double? AverageRating { get; set; }
     }
 }
