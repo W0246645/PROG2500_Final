050681c101f310b41cc5531a62cb42f7672060d2
Matthew Covey <90411582+W0246645@users.noreply.github.com>
2023-04-11 23:50:55 -0300

    Trimmed some code and significantly improved load time/ram usage.

--- PROG2500_Final/PROG2500_Final/Pages/MoviesPage.xaml.cs
+++ PROG2500_Final/PROG2500_Final/Pages/MoviesPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using PROG2500_Final.Data;
+using PROG2500_Final.Models;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -28,45 +30,66 @@ namespace PROG2500_Final.Pages
             LoadMovies(SearchTextBox.Text);
         }
 
-        private void LoadMovies(string searchTitle = null)
+        private void LoadMovies(string searchTitle = "")
         {
-            Movies.Clear();
-
-            var titles = _context.Titles.ToList();
-            if (!string.IsNullOrEmpty(searchTitle))
+            if (searchTitle != "")
             {
-                titles = titles.Where(t => t.PrimaryTitle.Contains(searchTitle)).ToList();
-            }
-
-            foreach (var title in titles)
-            {
-                var movieData = new MovieData
-                {
-                    Title = title.PrimaryTitle
-                };
-
-                // Fetch average rating from Ratings table using titleId
-                var rating = _context.Ratings.FirstOrDefault(r => r.TitleId == title.TitleId);
-                if (rating != null)
-                {
-                    movieData.AverageRating = (double?)rating.AverageRating;
-                }
-
-                // Fetch director's name from Principals table using titleId and job_category
-                var principal = _context.Principals.FirstOrDefault(p => p.TitleId == title.TitleId && p.JobCategory == "director");
-                if (principal != null)
-                {
-                    var name = _context.Names.FirstOrDefault(n => n.NameId == principal.NameId && principal.JobCategory == "director");
-                    if (name != null)
+                MoviesListView.ItemsSource = _context.Titles
+                    .Where(t => t.PrimaryTitle.Contains(searchTitle))
+                    .Select(t => new MovieData
                     {
-                        movieData.Director = name.PrimaryName;
-                    }
-                }
-
-                Movies.Add(movieData);
+                        Title = t.PrimaryTitle,
+                        Director = t.Names.Where(n => n.Principals.First(p => p.JobCategory == "director") != null).First().PrimaryName,
+                        AverageRating = (double?)t.Rating.AverageRating
+                    }).Take(100).ToList();
+            } 
+            else
+            {
+                MoviesListView.ItemsSource = _context.Titles
+                    .Take(1000)
+                    .Select(t => new MovieData 
+                    {
+                        Title = t.PrimaryTitle,
+                        Director = t.Names.Where(n => n.Principals.First(p => p.JobCategory == "director") != null).First().PrimaryName,
+                        AverageRating = (double?)t.Rating.AverageRating
+                    }).ToList();
             }
 
-            MoviesListView.ItemsSource = Movies;
+
+
+            //var titles = _context.Titles.ToList();
+            //if (!string.IsNullOrEmpty(searchTitle))
+            //{
+            //    titles = titles.Where(t => t.PrimaryTitle.Contains(searchTitle)).ToList();
+            //}
+
+            //foreach (var title in titles)
+            //{
+            //    var movieData = new MovieData
+            //    {
+            //        Title = title.PrimaryTitle
+            //    };
+
+            //    // Fetch average rating from Ratings table using titleId
+            //    var rating = _context.Ratings.FirstOrDefault(r => r.TitleId == title.TitleId);
+            //    if (rating != null)
+            //    {
+            //        movieData.AverageRating = (double?)rating.AverageRating;
+            //    }
+
+            //    // Fetch director's name from Principals table using titleId and job_category
+            //    var principal = _context.Principals.FirstOrDefault(p => p.TitleId == title.TitleId && p.JobCategory == "director");
+            //    if (principal != null)
+            //    {
+            //        var name = _context.Names.FirstOrDefault(n => n.NameId == principal.NameId && principal.JobCategory == "director");
+            //        if (name != null)
+            //        {
+            //            movieData.Director = name.PrimaryName;
+            //        }
+            //    }
+
+            //    Movies.Add(movieData);
+            //    }
         }
     }
 
